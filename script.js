a7cdec80adfadf2dfdadbaf01364708d57e180e5
Luiseeop <luiseeop1@gmail.com>
2024-10-18 22:02:15 +0200

    Revert "Add files via upload"

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('submit-button').onclick = function() {
-    const name = document.getElementById('name-input').value;
-    document.getElementById('output').innerText = `Bonjour, ${name}!`;
-};
